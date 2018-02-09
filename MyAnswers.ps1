d616895a0e1f76a76ff898eda703bad9c3ff19bf
Codespace Blog <35943153+codespaceblog@users.noreply.github.com>
2018-02-09 18:09:18 -0500

    Add files via upload
    Uploaded my answers

---
+++ MyAnswers.ps1
@@ -0,0 +1,21 @@
+$myData = Import-Csv C:\Users\jamuldr\networkdata.csv
+
+# Question 1 
+$myData
+
+# Question 2 
+$myData.length
+
+# Question 3
+$myData | ForEach-Object { if ($_.password.length -lt 30){ $_ }}
+
+# Question 4
+$myData | ForEach-Object { if ($_.password.length -lt 30){ $_.password }}
+
+# Question 5
+$myData | ForEach-Object {if ($_."user name" -eq "Koressa") { $_ } }
+
+# Question 6
+$myData | ForEach-Object {if ($_."host ip address" -eq "10.10.16.74") { $_ } }
+
+
